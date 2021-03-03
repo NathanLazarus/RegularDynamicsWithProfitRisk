41154bbd089067a578da9d09e1335531291e369a
NathanLazarus <nathanyanks3@gmail.com>
2021-03-03 10:42:46 -0700

    Making Phi a State Variable

--- SchmittGroheUribeHelperFunctions/get_coefs_SchmittGrohe_Uribe.m
+++ SchmittGroheUribeHelperFunctions/get_coefs_SchmittGrohe_Uribe.m
@@ -4,10 +4,13 @@ k=KSTAR; c=CSTAR; l=LSTAR; Z=ZSTAR; Z1 = ZSTAR; Z2 = ZSTAR; Z3 = ZSTAR; P = PSTA
 kp=k; cp=c; lp=l; Zp=Z; Z1p = Z; Z2p = Z; Z3p = Z; Pp = P; riskless_r_ = RSTAR;
 stockSTAR = G*((PSTAR-1)/PSTAR)*y_func(KSTAR,LSTAR,ZSTAR,ALFA,PHISTAR)/((1+RSTAR) - G); stock = stockSTAR; stockp = stockSTAR;
 risky_r_ = RSTAR; risky_rp_ = RSTAR;
-phi = PHISTAR; phip = PHISTAR; lhat = LSTAR; lhatp = LSTAR;
-phi_t = PHISTAR; phihatp = PHISTAR; phi_tminus1 = PHISTAR; phihatp = PHISTAR;
 
-eta     = [0 0 0; 1 0 0; 0 0 0; 0 0 0; 0 0 0; 0 sigma_P/sigma_Z 0; 0 0 sigma_phi/sigma_Z]; %Matrix defining driving force
+lhat = LSTAR; phi = PHISTAR;  phi_tminus1 = PHISTAR;      phi_tminus2 = PHISTAR;      phihat = PHISTAR; P_lag = PSTAR;
+
+lhatp = lhat; phip = PHISTAR; phi_tminus1p = phi_tminus1; phi_tminus2p = phi_tminus2; phihatp = phihat; P_lagp = PSTAR;
+
+
+eta     = [0 0 0; 1 0 0; 0 0 0; 0 0 0; 0 0 0; 0 sigma_P/sigma_Z 0; 0 0 sigma_phi/sigma_Z; 0 0 0; 0 0 0]; %Matrix defining driving force
 
 flatten = @(A) A(:);
 
@@ -29,18 +32,20 @@ if order >= 2
 
     [gss,hss] = gss_hss(nfx,nfxp,nfy,nfyp,nfypyp,nfypy,nfypxp,nfypx,nfyyp,nfyy,nfyxp,nfyx,nfxpyp,nfxpy,nfxpxp,nfxpx,nfxyp,nfxy,nfxxp,nfxx,hx,gx,gxx,eta);
 
-    dec_l_hat =[LSTAR,gx(1,:),1/2*flatten(gxx(1,:,:))',1/2*gss(1)];
+    dec_l_hat = [LSTAR,gx(1,:),1/2*flatten(gxx(1,:,:))',1/2*gss(1)];
+    dec_phi_hat = [PHISTAR,gx(3,:),1/2*flatten(gxx(3,:,:))',1/2*gss(3)];
 
 else
-    dec_l_hat =[LSTAR,gx(1,:),zeros([1 nstate^2+1])];
+    dec_l_hat = [LSTAR,gx(1,:),zeros([1 nstate^2+1])];
+    dec_phi_hat = [PHISTAR,gx(3,:),zeros([1 nstate^2+1])];
 
 end
 
 
-eta     = [0 0 0; 1 0 0; 0 0 0; 0 0 0; 0 0 0; 0 sigma_P/sigma_Z 0; 0 0 sigma_phi/sigma_Z; 0 0 0]; %adding lhat state variable
+eta     = [0 0 0; 1 0 0; 0 0 0; 0 0 0; 0 0 0; 0 sigma_P/sigma_Z 0; 0 0 sigma_phi/sigma_Z; 0 0 0; 0 0 0]; %adding lhat state variable but removing P_lag
 
 
-[fx,fxp,fy,fyp,fypyp,fypy,fypxp,fypx,fyyp,fyy,fyxp,fyx,fxpyp,fxpy,fxpxp,fxpx,fxyp,fxy,fxxp,fxx,f] = stage2model(u, dec_l_hat, decision_func_to_use, ssvals);
+[fx,fxp,fy,fyp,fypyp,fypy,fypxp,fypx,fyyp,fyy,fyxp,fyx,fxpyp,fxpy,fxpxp,fxpx,fxyp,fxy,fxxp,fxx,f] = stage2model(u, dec_l_hat, dec_phi_hat, decision_func_to_use, ssvals_stage1);
 
 %Obtain numerical derivatives of f
 num_eval
--- SchmittGroheUribeHelperFunctions/stage1model.m
+++ SchmittGroheUribeHelperFunctions/stage1model.m
@@ -1,8 +1,7 @@
-% model1.M
 function [fx,fxp,fy,fyp,fypyp,fypy,fypxp,fypx,fyyp,fyy,fyxp,fyx,fxpyp,fxpy,fxpxp,fxpx,fxyp,fxy,fxxp,fxx,f] = stage1model(u)
 
-syms DELTA ALFA BETTA G LAMBDAZ ETA MU LAMBDAP LAMBDAphi GAMA SIGM
-syms c cp l lp k kp Z Zp Z1 Z1p Z2 Z2p Z3 Z3p P Pp phi_tminus1 phi_t phihat phihatp % riskless_r_ riskless_rp_ risky_rp_ risky_r_
+syms DELTA ALFA BETTA G LAMBDAZ ETA MU LAMBDAP LAMBDAphi LAMBDAphi_lagged phi_reg_intercept LAMBDAphi_theta LAMBDAphi_lagtheta GAMA SIGM
+syms c cp l lp k kp Z Zp Z1 Z1p Z2 Z2p Z3 Z3p P_lag P_lagp P Pp phi_tminus2 phi_tminus2p phi_tminus1 phi_tminus1p phihat phihatp % riskless_r_ riskless_rp_ risky_rp_ risky_r_
 
 if ~exist('u','var')
     u = (1/(1 - SIGM)) * c^(1-SIGM) * (1 - (GAMA/(1+ETA)) * l^(1+ETA));
@@ -13,7 +12,7 @@ dupdcp = jacobian(up,cp);
 dudc = jacobian(u,c);
 %dudl = jacobian(u,l);
 
-f1 = c + G*kp - (1-DELTA) * k - y_func(k, l, Z, ALFA, phi_t);
+f1 = c + G*kp - (1-DELTA) * k - y_func(k, l, Z, ALFA, phi_tminus1p);
 f2 = dudc - BETTA * subs(dupdcp, cp, G*cp) * big_R(kp, lp, Pp, Zp, ALFA, DELTA, phihatp);
 f3 = laborsupply(u) + w_func(k, l, P, Z, ALFA, phihat);
 f4 = Zp - Z^LAMBDAZ;
@@ -24,14 +23,16 @@ f8 = Z3p - Z2;
 % f9 = riskless_r_ - dudc/(BETTA * subs(dupdcp,cp,G*cp));
 % f10 = dudc - BETTA * subs(dupdcp, cp, G*cp) * G * (stockp + ((Pp-1)/Pp)*y_func(kp, lp, Zp, ALFA, phi two shocks in the future))/stock;
 % f11 = risky_r_ - big_R(k,l,P,Z,ALFA,DELTA);
-f_phi_AR = phi_t - phi_transition(phi_tminus1, P);
-f_phihat = phihat - phi_transition(phi_tminus1, P);
+f_phi_AR = phi_tminus1p - phi_transition(phi_tminus1, phi_tminus2, P, P_lag, LAMBDAphi, LAMBDAphi_lagged, phi_reg_intercept, LAMBDAphi_theta, LAMBDAphi_lagtheta, 0);
+f_phihat = phihat - phi_transition(phi_tminus1, phi_tminus2, P, P_lag, LAMBDAphi, LAMBDAphi_lagged, phi_reg_intercept, LAMBDAphi_theta, LAMBDAphi_lagtheta, 0);
+f_phi_lag = phi_tminus2p - phi_tminus1;
+f_P_lag = P_lagp - P;
 
-f = [f1;f2;f3;f4;f5;f6;f7;f8;f_phi_AR];
+f = [f1;f2;f3;f4;f5;f6;f7;f8;f_phi_AR;f_phihat;f_phi_lag;f_P_lag];
 
-x = [k Z Z1 Z2 Z3 P phi_tminus1];
+x = [k Z Z1 Z2 Z3 P phi_tminus1 phi_tminus2 P_lag];
 y = [l c phihat];
-xp = [kp Zp Z1p Z2p Z3p Pp phi_t];
+xp = [kp Zp Z1p Z2p Z3p Pp phi_tminus1p phi_tminus2p P_lagp];
 yp = [lp cp phihatp];
 
 [fx,fxp,fy,fyp,fypyp,fypy,fypxp,fypx,fyyp,fyy,fyxp,fyx,fxpyp,fxpy,fxpxp,fxpx,fxyp,fxy,fxxp,fxx]=anal_deriv(f,x,y,xp,yp);
